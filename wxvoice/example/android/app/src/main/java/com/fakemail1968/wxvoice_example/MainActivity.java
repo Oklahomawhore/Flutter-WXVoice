bc5d23b0ff6ae0b47a25420f617da5494446ab61
wangshu <killnobody0514@gmail.com>
2019-06-26 16:31:56 +0800

    first commit

---
+++ wxvoice/example/android/app/src/main/java/com/fakemail1968/wxvoice_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fakemail1968.wxvoice_example;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
